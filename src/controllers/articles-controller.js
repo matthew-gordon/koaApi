5520680c5f9ee14f0bb67adeff91cd5766933c58
Matthew Gordon <laxredemption5525@gmail.com>
2017-07-23 13:28:43 -0600

    Add articles controller/router skeleton

---
+++ src/controllers/articles-controller.js
@@ -0,0 +1,11 @@
+// const slug = require('slug')
+// const uuid = require('uuid')
+// const humps = require('humps')
+// const _ = require('lodash')
+// const {ValidationError} = require('lib/errors')
+//
+// const {getSelect} = require('lib/utils')
+// const {articleFields, userFields, relationMaps} = require('lib/relations-map')
+// const joinJs = require('join-js').default
+
+// module.exports = {}
---
+++ src/routes/articles-router.js
@@ -0,0 +1,5 @@
+// const Router = require('koa-router')
+// const ctrl = require('controllers').articles
+// const router = new Router()
+//
+// router.param('slug', ctrl.bySlug)
